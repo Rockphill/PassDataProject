d038b07463dff21bf93c94232daace1b8ab165ac
Andrei <macwin33@gmail.com>
2022-09-17 22:30:08 +0300

    add segue

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Andrei Sergienko on 17.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var login: String?
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else { return }
+        label.text  = "Hello, \(login)"
+    }
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+}
